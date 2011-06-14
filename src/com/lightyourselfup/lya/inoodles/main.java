fef7723d5c69ad413b9240962e32a072d15bd946
Luis Rodriguez <luisone_dtmb@hotmail.com>
2011-06-15 00:17:28 +0200

    primera version q medio funciona de activity2activity
    Change-Id: I56c4ae54653850228f6e56ac711b77d2b51f8437

--- src/com/lightyourselfup/lya/inoodles/main.java
+++ src/com/lightyourselfup/lya/inoodles/main.java
@@ -1,7 +1,9 @@
 package com.lightyourselfup.lya.inoodles;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class main extends Activity {
     /** Called when the activity is first created. */
@@ -10,4 +12,11 @@ public class main extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+	public void onReviewClick(View button){
+		
+		Intent intent = new Intent(); // creas un Intent vacio(new)
+		intent.setClass(this, review.class);
+		startActivity(intent);
+	}
 }
--- src/com/lightyourselfup/lya/inoodles/review.java
+++ src/com/lightyourselfup/lya/inoodles/review.java
@@ -1,7 +1,9 @@
 package com.lightyourselfup.lya.inoodles;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class review extends Activity {
     /** Called when the activity is first created. */
@@ -10,4 +12,16 @@ public class review extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.review);
     }
+    
+	public void onMainClick(View button){
+		
+		Intent intent = new Intent(); // creas un Intent vacio(new)
+		intent.setClass(this, main.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		//If set, and the activity being launched is already running in the current task, 
+		//then instead of launching a new instance of that activity, all of the other activities 
+		//on top of it will be closed and this Intent will be delivered to the (now on top) old 
+		//activity as a new Intent.
+		startActivity(intent);
+	}
 }
